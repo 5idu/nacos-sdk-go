c202456f9197bb3c7be8e5c6aac26aacb15c1738
xugang <xugang@colipu.com>
2023-12-19 11:32:53 +0800

    update

---
+++ clients/config_client/config_proxy_interface.go
@@ -0,0 +1,19 @@
+package config_client
+
+import (
+	"context"
+
+	"github.com/5idu/nacos-sdk-go/common/remote/rpc"
+	"github.com/5idu/nacos-sdk-go/common/remote/rpc/rpc_request"
+	"github.com/5idu/nacos-sdk-go/common/remote/rpc/rpc_response"
+	"github.com/5idu/nacos-sdk-go/model"
+	"github.com/5idu/nacos-sdk-go/vo"
+)
+
+type IConfigProxy interface {
+	queryConfig(dataId, group, tenant string, timeout uint64, notify bool, client *ConfigClient) (*rpc_response.ConfigQueryResponse, error)
+	searchConfigProxy(param vo.SearchConfigParam, tenant, accessKey, secretKey string) (*model.ConfigPage, error)
+	requestProxy(rpcClient *rpc.RpcClient, request rpc_request.IRequest, timeoutMills uint64) (rpc_response.IResponse, error)
+	createRpcClient(ctx context.Context, taskId string, client *ConfigClient) *rpc.RpcClient
+	getRpcClient(client *ConfigClient) *rpc.RpcClient
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/5idu/nacos-sdk-go
+
+go 1.15
+
+require (
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1704
+	github.com/buger/jsonparser v1.1.1
+	github.com/golang/mock v1.6.0
+	github.com/golang/protobuf v1.5.2
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.12.2
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/zap v1.21.0
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
+	golang.org/x/time v0.0.0-20220722155302-e5dcc9cfc0b9
+	google.golang.org/grpc v1.48.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
